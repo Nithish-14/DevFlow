e6aafb8c8fa4d5405cfe3800cd6762cdca34ae76
Nithish <nithishm612345@gmail.com>
2024-07-17 22:11:27 +0530

    sidebar update

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,7 +4,7 @@ import LeftSidebar from "../../../components/shared/LeftSidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="background-light850_dark100 relative custom-scrollbar">
+    <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
         <LeftSidebar />
