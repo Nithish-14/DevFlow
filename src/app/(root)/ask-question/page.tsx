5ca7c4d1a3644c09ced7c01d69a4da8bfdd65e8a
Nithish <nithishm612345@gmail.com>
2024-07-20 22:14:02 +0530

    ask question page

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,7 +1,17 @@
+import Question from "../../../../components/forms/Question";
+
 import React from "react";
 
-const Page = () => {
-  return <div>Ask Question</div>;
+const Page = async () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
