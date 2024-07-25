c9766b2b42a4898897a2bd1bbd53d2929eb77184
Nithish <nithishm612345@gmail.com>
2024-07-25 15:14:07 +0530

    webhook update

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,9 +11,16 @@ export default authMiddleware({
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhooks", "/api/chatgpt"],
+  ignoredRoutes: ["/api/webhooks(.*)", "/api/chatgpt"],
 });
 
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
