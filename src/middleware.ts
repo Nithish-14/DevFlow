c74f22b7e54208286719f5a66f2d9f814199ea89
Nithish <nithishm612345@gmail.com>
2024-07-25 19:09:08 +0530

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/webhooks",
-    "question/:id",
-    "/tags",
-    "/tags/:id",
-    "/profile/:id",
-    "/community",
-    "/jobs",
-  ],
+  // publicRoutes: [
+  //   "/",
+  //   "/api/webhooks",
+  //   "question/:id",
+  //   "/tags",
+  //   "/tags/:id",
+  //   "/profile/:id",
+  //   "/community",
+  //   "/jobs",
+  // ],
   ignoredRoutes: ["/api/webhooks(.*)", "/api/chatgpt"],
 });
 
